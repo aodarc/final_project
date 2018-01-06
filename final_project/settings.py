3be8a0d21501a8e2e63a6ea247b79518000d3e67
kotus07 <kotisden@gmail.com>
2018-01-06 13:08:54 +0200

    my_progect

--- final_project/settings.py
+++ final_project/settings.py
@@ -115,4 +115,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static'
+
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, ""),
+#   )
+
+
