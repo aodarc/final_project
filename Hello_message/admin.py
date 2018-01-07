7455b53e4f9845df6660fb718f6b92d76137c9fa
kotus07 <kotisden@gmail.com>
2018-01-07 14:30:08 +0200

    S-15 create model from schema

--- Hello_message/admin.py
+++ Hello_message/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from Hello_message.models import (
+    User,
+    City,
+    Contry_id
+)
 
+admin.site.register(User)
+admin.site.register(City)
+admin.site.register(Contry_id)
--- Hello_message/models.py
+++ Hello_message/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class User(models.Model):
+    first_name = models.CharField(max_length=20, verbose_name='First_name')
+    age        = models.BigIntegerField(verbose_name='Age')
+    phone      = models.BigIntegerField(verbose_name='Phone')
+    city       = models.ForeignKey(to='City')
+    stret      = models.CharField(max_length=50, verbose_name='Stret')
+    contry_id  = models.ForeignKey(to='Contry_id')
+
+
+class Contry_id(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Name')
+    area = models.BigIntegerField(verbose_name='Area')
+
+class City(models.Model):
+    name           = models.CharField(max_length=20, verbose_name='Name')
+    area           = models.BigIntegerField(max_length=20, verbose_name='Area')
+    biggest_street = models.CharField(max_length=20, verbose_name='Biggest street')
+
+
 # Create your models here.
