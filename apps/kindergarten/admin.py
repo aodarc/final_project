420359630c0fb7279310c7e57a809e24c38d2771
riaboshtan <sasha.riaboshtan@gmail.com>
2018-01-16 20:56:04 +0200

    register two groups: Kindergarten and group

--- apps/kindergarten/admin.py
+++ apps/kindergarten/admin.py
@@ -1,1 +1,7 @@
 # Register your models here.
+from django.contrib import admin
+
+from apps.kindergarten.models import Kindergarden, Group
+
+admin.site.register(Kindergarden)
+admin.site.register(Group)
