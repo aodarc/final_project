9daf9538e4f3a619dd9b3b2464e4d8ed30e844b0
riaboshtan <sasha.riaboshtan@gmail.com>
2018-01-16 19:00:32 +0200

    added url of page with m2m

--- final_project/urls.py
+++ final_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from baseinf.views import fp
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^base page/$', fp)
 ]
