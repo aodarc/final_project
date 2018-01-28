e21b75d3ab8dcf162763ae824f420a820b5670d4
andriy <maziar_vedmidne@ukr.net>
2018-01-28 15:02:26 +0200

    I_can`t_created_makemigrations_i_have_an_error

--- apps/location/admin.py
+++ apps/location/admin.py
@@ -1,1 +1,6 @@
 # Register your models here.
+from django.contrib import admin
+from apps.location.models import Address
+
+
+admin.site.register(Address)
--- apps/location/models.py
+++ apps/location/models.py
@@ -1,1 +1,45 @@
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Address(models.Model):
+    DISTRICT_CHOICE = (
+        ('Шевченківський'),
+        ('Галицький'),
+        ('Франківський'),
+        ('Залізничний'),
+        ('Сихівський'),
+        ('Личаківський'),
+        )
+
+    country = models.CharField(
+        unique= True,
+        max_length=255,
+        blank=False,
+        verbose_name='Країна'
+    )
+    city = models.CharField(
+        unique= True,
+        max_length=122,
+        blank=False,
+        verbose_name='Місто'
+    )
+    Address = models.OneToOneField(to=country)  # how to do it better?
+    district = models.CharField(
+        max_length= 22,
+        choices=DISTRICT_CHOICE,
+        blank=False,
+        verbose_name='Район'
+    )
+    street = models.CharField(
+        max_length= 122,
+        blank=False,
+        verbose_name='Вулиця'
+    )
+
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Місцезнаходження дитячого садка'
