6ed89f7292fd245014fb6f17ce4880ed36ba2927
andriy <maziar_vedmidne@ukr.net>
2018-02-01 22:17:07 +0200

    fix-kindergarten.urls

--- apps/kindergarten/urls.py
+++ apps/kindergarten/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from django.conf.urls import url, include
-
 from apps.kindergarten.views import Kindergartenview
-from final_project.views import MainPageView
+
 
 urlpatterns = [
     url(r'^$', Kindergartenview.as_view()),
-]
+    ]
