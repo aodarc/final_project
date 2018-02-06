ee3f06e25a5e44957fc761ae860a2e0005db60e4
riaboshtan <sasha.riaboshtan@gmail.com>
2018-02-06 20:56:26 +0200

    add func to view

--- apps/kindergarten/views.py
+++ apps/kindergarten/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from apps.kindergarten.models import (
+    Kindergarten,
+    Group
+)
+from apps.kindergarten.templates import *
+
+from django.views.generic.detail import DetailView
+
+class KinderView(DetailView):
+    template_name = 'show_kindergarten.html'
+    model = Kindergarten
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data()
+
+        context['groups'] = Group.objects.filter(kindergarten=self.object)
+
+        return context
