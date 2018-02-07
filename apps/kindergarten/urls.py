8e6fb334c7c07ceab1c860884214f6458748dd9f
riaboshtan <sasha.riaboshtan@gmail.com>
2018-02-07 19:12:24 +0200

    add name to url

--- apps/kindergarten/urls.py
+++ apps/kindergarten/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from apps.kindergarten.views import KinderView
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)$', KinderView.as_view()),
+    url(r'^(?P<pk>[0-9]+)$', KinderView.as_view(), name='show-kindergarten'),
 ]
