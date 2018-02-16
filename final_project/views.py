24e89281776985267db109a44705576a59f9e112
andriy <maziar_vedmidne@ukr.net>
2018-02-16 14:07:12 +0200

    i-need-get-shape-in the-image

--- final_project/views.py
+++ final_project/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import TemplateView
-
+from apps.kindergarten.models import Kindergarten
 
 class MainPageView(TemplateView):
     template_name = 'main_page/main_content.html'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, pk=None, **kwargs):
         context = super().get_context_data(**kwargs)
 
         context['title'] = "Our main page"
+        context['kinder_list'] = Kindergarten.objects.all()
         return context
 
