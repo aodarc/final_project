73ab2f0fede6b872f478131403c2b7cc9fde73e5
riaboshtan <sasha.riaboshtan@gmail.com>
2018-02-17 11:06:31 +0200

    add UserPageView

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,7 +1,11 @@
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import DetailView
 from django.views.generic.edit import FormView
+
+from apps.user_profile.models import UserProfile
 
 
 class RegisterFormView(FormView):
@@ -12,3 +16,15 @@ class RegisterFormView(FormView):
     def form_valid(self, form):
         form.save()
         return super(RegisterFormView, self).form_valid(form)
+
+
+class UserPageView(DetailView):
+    template_name = 'user_profile/user_page.html'
+    model = UserProfile
+
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data()
+        context ['user_profile'] = User.objects.all()
+
+        return context
