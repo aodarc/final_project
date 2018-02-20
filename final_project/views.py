d267b006d1a0c8ae5ba8c68614535726245ee646
andriy <maziar_vedmidne@ukr.net>
2018-02-20 15:09:18 +0200

    i-get-shape-in the-image

--- final_project/views.py
+++ final_project/views.py
@@ -4,7 +4,7 @@ from apps.kindergarten.models import Kindergarten
 class MainPageView(TemplateView):
     template_name = 'main_page/main_content.html'
 
-    def get_context_data(self, pk=None, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         context['title'] = "Our main page"
