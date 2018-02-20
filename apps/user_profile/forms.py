725fb8293cb87b87ffdd50cfae467fbfad9cb61a
riaboshtan <sasha.riaboshtan@gmail.com>
2018-02-20 20:32:34 +0200

    exclude

--- apps/user_profile/forms.py
+++ apps/user_profile/forms.py
@@ -1,8 +1,12 @@
 from django import forms
+
+from apps.location.models import City
 from apps.user_profile.models import Child
+from django.db import models
 
 
 class RegisterChildForm(forms.ModelForm):
         class Meta:
             model = Child
-            fields = ('__all__')
+            #fields = '__all__'
+            exclude = ['parents', 'privilege', 'privilege_series', 'privilege_number']
