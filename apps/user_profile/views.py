ba1f8cac799b00123420321ca41aaf1a54a0b696
riaboshtan <sasha.riaboshtan@gmail.com>
2018-02-22 19:30:58 +0200

    new

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -19,17 +19,39 @@ class RegisterFormView(FormView):
         return super(RegisterFormView, self).form_valid(form)
 
 
+class RegisterChildFormView(FormView):
+    form_class = UserCreationForm
+    template_name = "main_page/register.html"
+    success_url = reverse_lazy('main')
 
-@login_required
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+
+
+
+
+#@login_required
 def child_form(request):
-    cities = City.objects.all()
-    if request.method == 'POST':
-        form = RegisterChildForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/user_profile/')
-        return render (request, 'add_child.html', {'form' : form, 'city_list': cities})
-    else:
-
-        form = RegisterChildForm()
-        return render(request, 'add_child.html', {'form' : form, 'city_list': cities})
+        cities = City.objects.all()
+        #data = {
+         #   'parents': request.user
+        #}initial={'parents': request}
+        #data.update(request.POST)
+        if request.method == 'POST':
+            form = RegisterChildForm(request.POST )
+            if form.is_valid():
+                #newform = form.save(commit=False)
+                #newform.user = request.user
+                form.save()
+                form.parents = request.user.id
+                form.save()
+                #newform.save()
+            #if form.is_valid():
+                #form.save()
+                return redirect('/user_profile/')
+            return render (request, 'add_child.html', {'form' : form, 'city_list': cities})
+        else:
+            form = RegisterChildForm()
+            return render(request, 'add_child.html', {'form' : form, 'city_list': cities})
