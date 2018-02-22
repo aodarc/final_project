6fa16449f3e81dfecfa948335bd048585d73f423
aodarc <odarchenko@ex.ua>
2018-02-22 19:54:03 +0200

    fixed FK to user in childForm

--- apps/user_profile/forms.py
+++ apps/user_profile/forms.py
@@ -1,15 +1,15 @@
 from django import forms
-from django.contrib.auth.models import User
 
-from apps.location.models import City
 from apps.user_profile.models import Child
-from django.db import models
 
 
 class RegisterChildForm(forms.ModelForm):
-    parents = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
     class Meta:
-            model = Child
-            #fields = '__all__'
-            exclude = ['privilege', 'privilege_series', 'privilege_number']
+        model = Child
+        # fields = '__all__'
+        exclude = ['privilege', 'privilege_series', 'privilege_number']
 
+    def __init__(self, user=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['parents'].initial = user
--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,13 +1,11 @@
 # Create your views here.
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from django.views.generic.edit import FormView
-from apps.location.models import City
-from apps.user_profile.models import Child
 
 from apps.user_profile.forms import RegisterChildForm
+
 
 class RegisterFormView(FormView):
     form_class = UserCreationForm
@@ -26,32 +24,17 @@ class RegisterChildFormView(FormView):
 
     def form_valid(self, form):
         form.save()
-        return super(RegisterFormView, self).form_valid(form)
+        return super().form_valid(form)
 
 
-
-
-
-#@login_required
+# @login_required
 def child_form(request):
-        cities = City.objects.all()
-        #data = {
-         #   'parents': request.user
-        #}initial={'parents': request}
-        #data.update(request.POST)
-        if request.method == 'POST':
-            form = RegisterChildForm(request.POST )
-            if form.is_valid():
-                #newform = form.save(commit=False)
-                #newform.user = request.user
-                form.save()
-                form.parents = request.user.id
-                form.save()
-                #newform.save()
-            #if form.is_valid():
-                #form.save()
-                return redirect('/user_profile/')
-            return render (request, 'add_child.html', {'form' : form, 'city_list': cities})
-        else:
-            form = RegisterChildForm()
-            return render(request, 'add_child.html', {'form' : form, 'city_list': cities})
+    if request.method == 'POST':
+        form = RegisterChildForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/user_profile/')
+        return render(request, 'add_child.html', {'form': form})
+    else:
+        form = RegisterChildForm(user=request.user)
+        return render(request, 'add_child.html', {'form': form})
