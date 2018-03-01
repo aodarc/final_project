5bb1a9c9ce2e987843a1bca87729607c33add111
riaboshtan <sasha.riaboshtan@gmail.com>
2018-03-01 20:14:26 +0200

    add parents

--- apps/user_profile/forms.py
+++ apps/user_profile/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from apps.kindergarten.models import Kindergarten
 from apps.user_profile.models import Child
@@ -22,9 +23,6 @@ class RegisterChildForm(forms.ModelForm):
     class Meta:
         model = Child
         fields = '__all__'
-        #exclude = ['privilege', 'privilege_series', 'privilege_number']
 
     def __init__(self, user=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if user:
-            self.fields['parents'].initial = user
